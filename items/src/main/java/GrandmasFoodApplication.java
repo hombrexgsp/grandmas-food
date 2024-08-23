54cd80508f2623e0489ab10d69d0a0fb6ea38371
Gaby <gabriel.santana@globant.com>
2024-08-22 20:41:20 -0500

    🎉 Project started!
    Added mock main entry points to the auth and items modules

---
+++ items/src/main/java/GrandmasFoodApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandmasFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandmasFoodApplication.class, args);
+	}
+
+}

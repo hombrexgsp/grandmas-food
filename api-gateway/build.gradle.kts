f9ffa42154a596437ea513b3cc1697142cde4ee1
Gaby <gabriel.santana@globant.com>
2024-08-22 20:29:40 -0500

    🔧 Added Gradle configuration files

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.globant"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    compileOnly(Libraries.lombok)
+    annotationProcessor(Libraries.lombok)
+
+    implementation(Libraries.springboot)
+    implementation(Libraries.springbootGraphQl)
+
+    testImplementation(Testing.springWebflux)
+    testImplementation(Testing.springbootStarter)
+    testImplementation(Testing.springbootGraphQl)
+
+
+    testRuntimeOnly(Testing.junit)
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "grandmas-food"
+include("auth")
+include("common")
+include("shopping-cart")
+include("items")
+include("payment")
+include("api-gateway")

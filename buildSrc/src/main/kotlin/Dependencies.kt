a8442e1aa0150e273e9db5ba77addb0c37521868
Gaby <gabriel.santana@globant.com>
2024-08-22 20:30:40 -0500

    ➕ Added dependencies to `buildSrc` directory

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,13 @@
+object Libraries {
+    const val lombok = "org.projectlombok:lombok"
+    const val springboot = "org.springframework.boot:spring-boot-starter"
+    const val spingbootDockerCompose = "org.springframework.boot:spring-boot-docker-compose"
+    const val springbootGraphQl = "org.springframework.boot:spring-boot-starter-graphql"
+}
+
+object Testing {
+    const val springbootGraphQl = "org.springframework.graphql:spring-graphql-test"
+    const val springWebflux = "org.springframework:spring-webflux"
+    const val springbootStarter = "org.springframework.boot:spring-boot-starter-test"
+    const val junit = "org.junit.platform:junit-platform-launcher"
+}

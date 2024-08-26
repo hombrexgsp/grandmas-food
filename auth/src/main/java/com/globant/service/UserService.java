008828c8b2539d6ce27670ae472256baa79d6713
jose.agudo <jose.agudo@globant.com>
2024-08-26 17:39:31 -0500

    I added some dependencies for the Auth subproject and also created the models package. If its ok with you guys, i'll handle the auth part. But pls, check it out and give me feedback, because i want to add security.

--- auth/src/main/java/com/globant/service/UserService.java
+++ auth/src/main/java/com/globant/service/UserService.java
@@ -1,10 +1,12 @@
 package com.globant.service;
 
 import com.globant.dto.UserDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface UserService {
 
     UserDto createUser(UserDto userDto);

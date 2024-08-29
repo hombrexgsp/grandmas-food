3f03ac9e0e3dfc86e678bdc1be89740598cda5e8
Gaby <gabriel.santana@globant.com>
2024-08-28 23:42:43 -0500

    🏷️ Added `ComboEntity` to module `products`

---
+++ products/src/main/java/com/globant/domain/ComboEntity.java
@@ -0,0 +1,24 @@
+package com.globant.domain;
+
+import domain.combo.Category;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.UUID;
+
+@Document(collection = "combos")
+@Data
+public class ComboEntity {
+
+    @MongoId
+    private ObjectId id;
+    private final UUID uuid;
+    private final String fantasyName;
+    private final Category category;
+    private final String description;
+    private final Float price;
+    private final boolean available;
+
+}

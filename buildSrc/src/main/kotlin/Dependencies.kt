2568a34aca7aa3603e8dd106bd602b0d856b081e
Gaby <gabriel.santana@globant.com>
2024-09-06 21:09:24 -0500

    ➕ Added Mongo dependency (again)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,6 +4,7 @@ object Libraries {
     const val springbootDevtools = "org.springframework.boot:spring-boot-devtools"
     const val spingbootDockerCompose = "org.springframework.boot:spring-boot-docker-compose"
     const val springbootGraphQl = "org.springframework.boot:spring-boot-starter-graphql"
+    const val springbootMongo = "org.springframework.boot:spring-boot-starter-data-mongodb"
     const val springbootRedis = "org.springframework.boot:spring-boot-starter-data-redis"
     const val springbootValidation = "org.springframework.boot:spring-boot-starter-validation"
     const val springbootWeb = "org.springframework.boot:spring-boot-starter-web"

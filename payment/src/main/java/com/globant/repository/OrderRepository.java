8534cd2bb1175661d461125ce000e0f2e625ae3f
Gaby <gabriel.santana@globant.com>
2024-09-11 02:33:38 -0500

    👔 Added both repositories for Order and Delivery

--- payment/src/main/java/com/globant/repository/OrderRepository.java
+++ payment/src/main/java/com/globant/repository/OrderRepository.java
@@ -1,18 +1,20 @@
 package com.globant.repository;
 
-import com.globant.model.CreateOrder;
+import com.globant.domain.entity.DeliveryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import com.globant.model.entity.Order;
+import com.globant.domain.entity.OrderEntity;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query("SELECT o from Order o")
-    List<Order> getAllOrders();
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    Optional<OrderEntity> findByUuid(UUID orderId);
+
 
 
 

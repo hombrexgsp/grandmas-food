5a6d5d775b4dc38f7e11ef0f8daf4762caf21792
Gaby <gabriel.santana@globant.com>
2024-09-11 02:37:24 -0500

    👔 Added `OrderController`

--- payment/src/main/java/com/globant/controllers/OrderController.java
+++ payment/src/main/java/com/globant/controllers/OrderController.java
@@ -1,18 +1,22 @@
 package com.globant.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
-import com.globant.dto.OrderDto;
-import com.globant.model.CreateOrder;
+import com.globant.domain.order.CreatedOrder;
+import com.globant.domain.order.CreateOrder;
+import com.globant.domain.order.Order;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.globant.model.entity.Order;
 import com.globant.service.OrderService;
 
 @RestController
@@ -26,17 +30,33 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+
     @GetMapping
-    public ResponseEntity<List<Order>> getAllOrders() {
-        List<Order> orders = orderService.getAll();
-        return ResponseEntity.ok(orders);
+    public ResponseEntity<List<CreatedOrder>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAll());
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Order> getOrder(@PathVariable @UUID String uuid) {
+        return ResponseEntity.ok(orderService.getOrder(java.util.UUID.fromString(uuid)));
     }
 
     @PostMapping
-    public ResponseEntity<OrderDto>createOrder(@RequestBody CreateOrder createOrder){
-        log.debug(createOrder.toString());
+    public ResponseEntity<CreatedOrder>createOrder(@RequestBody @Valid CreateOrder createOrder){
+        log.info(createOrder.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(createOrder));
     }
 
-
+    @PatchMapping("/{uuid}/delivered/{timestamp}")
+    public ResponseEntity<CreatedOrder> markAsDelivered(
+            @PathVariable @UUID @NotNull String uuid,
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) String timestamp
+    ){
+        return ResponseEntity.ok(
+                orderService.setDelivery(
+                        java.util.UUID.fromString(uuid),
+                        LocalDateTime.from(DateTimeFormatter.ISO_DATE_TIME.parse(timestamp))
+                )
+        );
+    }
 }

a56a66633886fa32db5f44aa4a00c9a1e35f3ae1
Gaby <gabriel.santana@globant.com>
2024-09-11 23:25:48 -0500

    🏷️ Updated JSON serialization in types

--- common/src/main/java/domain/payment/delivery/Delivered.java
+++ common/src/main/java/domain/payment/delivery/Delivered.java
@@ -1,11 +1,16 @@
 package domain.payment.delivery;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record Delivered(LocalDateTime deliveredDate) implements Delivery {
+public record Delivered(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+        LocalDateTime deliveredTime
+) implements Delivery {
     @Override
     public String toString() {
-        return STR."Delivered in \{deliveredDate}";
+        return STR."Delivered in \{deliveredTime}";
     }
 }
 

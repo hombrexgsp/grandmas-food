7f0b12f0e004383ec08effc780c60da7bee2a384
Gaby <gabriel.santana@globant.com>
2024-09-24 19:50:03 -0500

    👔 Updated Payment resolvers to support WebFlux

--- api-gateway/src/main/java/com/globant/resolvers/PaymentResolver.java
+++ api-gateway/src/main/java/com/globant/resolvers/PaymentResolver.java
@@ -3,13 +3,15 @@ package com.globant.resolvers;
 import com.globant.domain.order.CheckoutOrder;
 import domain.payment.CreateOrder;
 import domain.payment.CreatedOrder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface PaymentResolver {
-    List<CreatedOrder> getAllOrders();
-    CreatedOrder checkout(CheckoutOrder checkoutOrder);
-    CreatedOrder deliverOrder(UUID orderId, LocalDateTime deliveryTime);
+    Flux<CreatedOrder> getAllOrders();
+    Mono<CreatedOrder> checkout(CheckoutOrder checkoutOrder);
+    Mono<CreatedOrder> deliverOrder(UUID orderId, LocalDateTime deliveryTime);
 }

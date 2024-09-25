4547a642493c3a93974d5b0987a9c1fa6d491224
Gaby <gabriel.santana@globant.com>
2024-09-24 19:47:08 -0500

    👔 Updated User resolvers to support WebFlux

--- api-gateway/src/main/java/com/globant/http/UserController.java
+++ api-gateway/src/main/java/com/globant/http/UserController.java
@@ -10,16 +10,13 @@ import domain.user.DocumentIdentity;
 import com.globant.domain.user.UpdateUserInput;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
-import java.util.Optional;
-
-@Slf4j
 @Controller
 public class UserController {
 
@@ -30,17 +27,17 @@ public class UserController {
     }
 
     @QueryMapping
-    public List<User> allUsers() {
+    public Flux<User> allUsers() {
         return userResolver.getAllUsers();
     }
 
     @QueryMapping
-    public User userByDocumentNumber(@Argument @NotEmpty String documentNumber) {
+    public Mono<User> userByDocumentNumber(@Argument @NotEmpty String documentNumber) {
         return userResolver.getUserByDocumentNumber(documentNumber);
     }
 
     @MutationMapping
-    public User createUser(@Argument @Valid CreateUserInput input){
+    public Mono<User> createUser(@Argument @Valid CreateUserInput input){
         User newUser = new User(
                 new DocumentIdentity(input.getDocumentIdentity().getDocumentType(), input.getDocumentIdentity().getDocumentNumber()),
                 input.getFirstName(),
@@ -54,36 +51,21 @@ public class UserController {
     }
 
     @MutationMapping
-    public User updateUser(@Argument @NotEmpty String documentNumber,
-                           @Argument(name = "input") @Valid UpdateUserInput updateUserInput){
-        User existingUser = userResolver.getUserByDocumentNumber(documentNumber);
+    public Mono<User> updateUser(
+            @Argument @NotEmpty String documentNumber,
+            @Argument(name = "input") @Valid UpdateUserInput updateUserInput
+    ) {
 
-        User updatedUser = new User(
-                existingUser.documentIdentity(),
-                Optional.ofNullable(updateUserInput.firstName()).orElse(existingUser.firstName()),
-                Optional.ofNullable(updateUserInput.lastName()).orElse(existingUser.lastName()),
-                Optional.ofNullable(updateUserInput.email()).orElse(existingUser.email()),
-                Optional.ofNullable(updateUserInput.phoneNumber()).orElse(existingUser.phoneNumber()),
-                Optional.ofNullable(updateUserInput.address()).orElse(existingUser.address())
-//                Optional.ofNullable(updateUserInput.firstName()).orElse(existingUser.firstName()),
-//                updateUserInput.lastName() != null ? updateUserInput.lastName() : existingUser.lastName(),
-//                updateUserInput.email() != null ? updateUserInput.email() : existingUser.email(),
-//                updateUserInput.phoneNumber() != null ? updateUserInput.phoneNumber() : existingUser.phoneNumber(),
-//                updateUserInput.address() != null ? updateUserInput.address() : existingUser.address()
-        );
-
-        log.info(updatedUser.toString());
-
-        return userResolver.updateUser(documentNumber, updatedUser);
+        return userResolver.updateUser(documentNumber, updateUserInput);
     }
 
     @MutationMapping
-    public boolean deleteUser(@Argument @NotEmpty String documentNumber){
+    public Mono<Boolean> deleteUser(@Argument @NotEmpty String documentNumber){
         return userResolver.deleteUser(documentNumber);
     }
 
     @QueryMapping
-    public List<User> userByFirstName(@Argument("firstName") @NotEmpty String firstName){
+    public Flux<User> userByFirstName(@Argument("firstName") @NotEmpty String firstName){
         return userResolver.getUserByFirstName(firstName);
     }
 
--- api-gateway/src/main/java/com/globant/resolvers/implementations/UserResolverImpl.java
+++ api-gateway/src/main/java/com/globant/resolvers/implementations/UserResolverImpl.java
@@ -1,67 +1,89 @@
 package com.globant.resolvers.implementations;
 
+import com.globant.domain.user.UpdateUserInput;
 import com.globant.domain.user.User;
 import com.globant.resolvers.UserResolver;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserResolverImpl implements UserResolver {
 
-    private final RestClient userClient;
+    private final WebClient userClient;
 
-    public UserResolverImpl(RestClient.Builder restClientBuilder) {
-        this.userClient = restClientBuilder.baseUrl("http://localhost:8081/users").build();
+    public UserResolverImpl(WebClient.Builder restClientBuilder) {
+        this.userClient = restClientBuilder
+                .baseUrl("http://localhost:8081/users")
+                .build();
     }
 
-    public User createUser(User newUser){
+    public Mono<User> createUser(User newUser){
         return userClient.post()
-                .uri("")
-                .body(newUser)
+                .body(Mono.just(newUser), User.class)
                 .retrieve()
-                .body(User.class);
+                .bodyToMono(User.class);
     }
 
-    public List<User> getAllUsers(){
-        return userClient.get().retrieve().body(new ParameterizedTypeReference<List<User>>() {
-        });
+    public Flux<User> getAllUsers(){
+        return userClient.get()
+                .retrieve()
+                .bodyToFlux(User.class);
     }
 
-    public User updateUser(String documentNumber, User updateUser){
+    public Mono<User> updateUser(String documentNumber, UpdateUserInput updateUserInput){
+        final var updatedUser = getUserByDocumentNumber(documentNumber)
+                .map( existingUser ->
+                        new User(
+                                existingUser.documentIdentity(),
+                                Optional.ofNullable(updateUserInput.firstName()).orElse(existingUser.firstName()),
+                                Optional.ofNullable(updateUserInput.lastName()).orElse(existingUser.lastName()),
+                                Optional.ofNullable(updateUserInput.email()).orElse(existingUser.email()),
+                                Optional.ofNullable(updateUserInput.phoneNumber()).orElse(existingUser.phoneNumber()),
+                                Optional.ofNullable(updateUserInput.address()).orElse(existingUser.address())
+
+                    )
+                );
+
         return userClient.put()
                 .uri("/{documentNumber}", documentNumber)
-                .body(updateUser, new ParameterizedTypeReference<User>() {})
+                .body(updatedUser, User.class)
                 .retrieve()
-                .body(User.class);
+                .bodyToMono(User.class);
     }
 
-    public boolean deleteUser(String documentNumber){
-            userClient.delete()
-                    .uri("/{documentNumber}", documentNumber)
-                    .retrieve()
-                    .toBodilessEntity();
-                    return true;
-
+    public Mono<Boolean> deleteUser(String documentNumber) {
+        return userClient.delete()
+                .uri("/{documentNumber}", documentNumber)
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        clientResponse -> Mono.error(new HttpClientErrorException(clientResponse.statusCode()))
+                )
+                .bodyToMono(Void.class)
+                .then(Mono.just(true))
+                .onErrorReturn(false);
     }
 
-    public User getUserByDocumentNumber(String documentNumber) {
+    public Mono<User> getUserByDocumentNumber(String documentNumber) {
         return userClient.get()
                 .uri("/document/{documentNumber}", documentNumber)
                 .retrieve()
-                .body(User.class);
+                .bodyToMono(User.class);
     }
 
-    public List<User> getUserByFirstName(String firstName){
+    public Flux<User> getUserByFirstName(String firstName){
         return userClient.get()
                 .uri("/name/{firstName}", firstName)
                 .retrieve()
-                .body(new ParameterizedTypeReference<List<User>>() {});
+                .bodyToFlux(User.class);
     }
-
-
-
 
 }
